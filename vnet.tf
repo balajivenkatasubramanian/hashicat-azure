6c4639a35fcad998eeabb8056a1453f64cf1427c
balajivenkatasubramanian <balaji.amritha@gmail.com>
2021-03-25 13:14:44 -0400

    Update vnet.tf

--- vnet.tf
+++ vnet.tf
@@ -1,6 +1,6 @@
 module "network" {
   source  = "app.terraform.io/balajimani/network/azurerm"
   version = "3.0.1"
-  esource_group_name = azurerm_resource_group.myresourcegroup.name
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
   
 }
